673b61ca6cd691fd49eebca322063f67264377f4
Justin Pember <justinsdevemail@gmail.com>
2019-02-12 09:16:28 -0500

    Outputted Video to Driver Station

--- src/main/include/Camera.h
+++ src/main/include/Camera.h
@@ -5,7 +5,11 @@ CAMERA PROGRAMMING, INCLUDES INIT, STREAMING, AND SETTING RES
 
 */
 #include <iostream>
+#include <frc/WPILib.h>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/core/core.hpp>
 #include "cscore.h"
+#include <cameraserver/CameraServer.h>
 
 //INITITATES / CONNECTS THE CAMERAS TO THE RIO, IF NO VALUES ARE SPECIFIED, DEFUALT CONSTRUCOTR WILL BE USED
 
@@ -16,37 +20,67 @@ namespace botVideo
 void StreamBotCameras(int resWidth, int res_height, int fps)
 {
     //defining camera, adds name and USB port
-    static cs::UsbCamera frontCamera{"camera1", 0};
+    cs::UsbCamera frontCamera{"camera1", 0};  
+    frontCamera = frc::CameraServer::GetInstance()->StartAutomaticCapture();
+
     //setting res
     frontCamera.SetResolution(resWidth, res_height);
     //setting frames per second
     frontCamera.SetFPS(fps);
 
-    //setting video mode for streaming
-    frontCamera.SetVideoMode(cs::VideoMode::kMJPEG, resWidth, res_height, fps);
-
     //creating sink to display vid
-    cs::VideoSink frontCameraSink{};
+    cs::CvSink frontCameraSink = frc::CameraServer::GetInstance()->GetVideo();
+
+    //creating stream to output video to be outputted to driver station
+    cs::CvSource vidStream = frc::CameraServer::GetInstance()->PutVideo("Gray", resWidth, res_height);
     frontCameraSink.SetSource(frontCamera);
 
-    
+//making mat array (images that are written in arrays)
+    cv::Mat source;
+    cv::Mat output;
+
+    //displaying video to driver station
+    while (true)
+    {
+        frontCameraSink.GrabFrame(source);
+        cvtColor(source, output, cv::COLOR_BGR2GRAY);
+        vidStream.PutFrame(source);
+    }
+
 }
+
+
 //SECOND CONSTRUCTOR | USES DEFAULT VALUES
 void StreamBotCameras()
 {
-    //defining camera, adds name and USB port
-    static cs::UsbCamera frontCamera{"camera1", 0};
+   //defining camera, adds name and USB port
+    cs::UsbCamera frontCamera{"camera1", 0};  
+    frontCamera = frc::CameraServer::GetInstance()->StartAutomaticCapture();
+
     //setting res
     frontCamera.SetResolution(640, 480);
     //setting frames per second
     frontCamera.SetFPS(15);
 
-    //setting video mode for streaming
-    frontCamera.SetVideoMode(cs::VideoMode::kMJPEG, 640, 480, 15);
-
     //creating sink to display vid
-    cs::VideoSink frontCameraSink{};
+    cs::CvSink frontCameraSink = frc::CameraServer::GetInstance()->GetVideo();
+
+    //creating stream to output video to be outputted to driver station
+    cs::CvSource vidStream = frc::CameraServer::GetInstance()->PutVideo("Gray", 640, 480);
     frontCameraSink.SetSource(frontCamera);
+
+//making mat array (images that are written in arrays)
+    cv::Mat source;
+    cv::Mat output;
+
+    //displaying video to driver station
+    while (true)
+    {
+        frontCameraSink.GrabFrame(source);
+        cvtColor(source, output, cv::COLOR_BGR2GRAY);
+        vidStream.PutFrame(source);
+    }
+
 }
 
-} // namespace botVideo
+}
