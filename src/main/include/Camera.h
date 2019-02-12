1683456d04bab45a5177757f82148a63ed14a9dc
Justin Pember <justinsdevemail@gmail.com>
2019-02-12 00:12:37 -0500

    Added Video Sink

--- src/main/include/Camera.h
+++ src/main/include/Camera.h
@@ -15,11 +15,23 @@ namespace botVideo
 //STREAMS THE CAMERA(S) TO THE DRIVER STATION
 void StreamBotCameras(int resWidth, int res_height, int fps)
 {
+    //defining camera, adds name and USB port
     static cs::UsbCamera frontCamera{"camera1", 0};
+    //setting res
     frontCamera.SetResolution(resWidth, res_height);
+    //setting frames per second
     frontCamera.SetFPS(fps);
+
+    //setting video mode for streaming
+    frontCamera.SetVideoMode(cs::VideoMode::kMJPEG, resWidth, res_height, fps);
+
+    //creating sink to display vid
+    cs::VideoSink frontCameraSink{};
+    frontCameraSink.SetSource(frontCamera);
+
+    
 }
-//SECOND CONSTRUCTOR | USES DEFUALT VALUES
+//SECOND CONSTRUCTOR | USES DEFAULT VALUES
 void StreamBotCameras()
 {
     //defining camera, adds name and USB port
@@ -28,6 +40,13 @@ void StreamBotCameras()
     frontCamera.SetResolution(640, 480);
     //setting frames per second
     frontCamera.SetFPS(15);
+
+    //setting video mode for streaming
+    frontCamera.SetVideoMode(cs::VideoMode::kMJPEG, 640, 480, 15);
+
+    //creating sink to display vid
+    cs::VideoSink frontCameraSink{};
+    frontCameraSink.SetSource(frontCamera);
 }
 
 } // namespace botVideo
